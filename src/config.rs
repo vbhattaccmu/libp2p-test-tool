870ed6d10e241e5f158f74ca858d4513de3e429c
Vikram Bhattacharjee <53374818+vbhattaccmu@users.noreply.github.com>
2023-11-13 07:37:32 +0800

    chore: add config and error

--- src/config.rs
+++ src/config.rs
@@ -1,1 +1,45 @@
 //! Config module for the tool.
+
+use libp2p::identity::{self, Keypair};
+
+#[derive(Clone)]
+pub struct Config {
+    /// Event timeout (in s)
+    pub timeout: u64,
+    /// Protocol name
+    pub protocol: String,
+    /// Swarm keypair
+    pub keypair: Keypair,
+    /// Swarm listening port
+    pub listening_port: u16,
+    /// DHT Bootstrap period (in s)
+    pub bootstrap_period: u64,
+    /// CSV file input for unreachable peers
+    pub unreachable_csv: String,
+    /// CSV file input for newly connected peers
+    pub just_connected: String,
+    /// Relay address for dialing  
+    pub bootstrap_addr: Vec<String>,
+    /// Log Level Setting
+    pub log_level: String,
+    /// Time duration till the tool operates (in s)
+    pub operation_duration: u64,
+}
+
+/// For convenience, default values are predefined
+impl Default for Config {
+    fn default() -> Self {
+        Config {
+            timeout: 20,
+            bootstrap_period: 5,
+            listening_port: 7072,
+            operation_duration: 181,
+            log_level: String::from("info"),
+            keypair: identity::Keypair::generate_ed25519(),
+            just_connected: String::from("/home/newly_connected.csv"),
+            protocol: String::from("/light-client-test/1.0.0"),
+            unreachable_csv: String::from("/home/unreachable.csv"),
+            bootstrap_addr: vec![String::from("/ip4/172.16.3.2/udp/39000/quic-v1")],
+        }
+    }
+}
--- src/error.rs
+++ src/error.rs
@@ -1,1 +1,27 @@
 //! Error handler for the tool.
+
+use std::fmt;
+
+#[derive(Debug)]
+pub enum CLIError {
+    IdentityError,
+    IPResolutionError,
+    ResourceBusy,
+    WriterError,
+}
+
+impl fmt::Display for CLIError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            CLIError::IdentityError => write!(f, "{}", IDENTITY_ERROR),
+            CLIError::IPResolutionError => write!(f, "{}", IP_RESOLUTION_ERROR),
+            CLIError::ResourceBusy => write!(f, "{}", RESOURCE_BUSY),
+            CLIError::WriterError => write!(f, "{}", WRITER_FLUSH_ERROR),
+        }
+    }
+}
+
+const IDENTITY_ERROR: &str = "Identity failed to parse the string to get a peer MultiAddress. Please check the input address format.";
+const IP_RESOLUTION_ERROR: &str = "Could not resolve IP from libp2p multiaddress.";
+const RESOURCE_BUSY: &str = " System resources busy. Please restart the client.";
+const WRITER_FLUSH_ERROR: &str = "Error occured while flusing buffer to file. Please check if your path is correct or if it has write access";
